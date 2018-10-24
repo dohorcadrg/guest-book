fd93671c5a6c48667803e05e90047716b1f96864
dohorcadrg <gloryleaspi@hotmail.com>
2018-10-24 17:00:49 +1000

    Inital

---
+++ client/states.js
@@ -0,0 +1,21 @@
+var guestBook = angular.module('guestBook', ['ui.router']);
+
+guestBook.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('recent-entries', {
+            url: '/',
+            templateUrl: 'client/recent-entries.html'
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('add-entry', {
+            url: '/add',
+            templateUrl: 'client/add-entry.html'     
+        });
+
+});
